ee4a9bd749f22e622d21c0df9620cda062fad0ca
nazmul-islam1412 <mohammad.nazmul.islam1412@gmail.com>
2025-11-10 01:00:57 +0600

    added Navbar,Footer,Jsons

--- src/Layouts/MainLayout.jsx
+++ src/Layouts/MainLayout.jsx
@@ -5,11 +5,13 @@ import { Outlet } from 'react-router';
 
 const MainLayout = () => {
     return (
-        <div>
+        <div className='flex flex-col min-h-screen'>
             <Navbar></Navbar>
-            <h1>Content</h1>
+           <div className='flex-1'>
+               <Outlet></Outlet>
+           </div>
             <Footer></Footer>
-            <Outlet></Outlet>
+            
         </div>
     );
 };
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,22 +3,31 @@ import App from "../App";
 import Home from "../Pages/Home";
 import Products from "../Pages/Products";
 import MainLayout from "../Layouts/MainLayout";
+import ErrorPage from "../Pages/ErrorPage";
+import Wishlist from "../Pages/Wishlist";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout></MainLayout>,
+    errorElement : <ErrorPage></ErrorPage>,
     children : [
         {
-    path: "/Home",
+    path: "/",
     element: <Home></Home>,
-  },
-    ]
+    loader : () => fetch('./FurnitureData.json')
   },
   
   {
     path: "/Products",
     element: <Products></Products>,
   },
+  {
+    path: "/Wishlist",
+    element: <Wishlist></Wishlist>,
+  },
+    ]
+  },
+  
 ]);
  export default router;
